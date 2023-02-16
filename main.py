8207da5aef6da70b042b101bb07e09d47b9d7e5a
Milmillones <124838122+Milmillones@users.noreply.github.com>
2023-02-16 02:46:51 +0000

    Leccion 04

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Hello")
+palabra="ithamar"
+
+# print(palabra[0])
+# print(palabra[1])
+# print(palabra[2])
+# print(palabra[3])
+# print(palabra[4])
+# print(palabra[5])
+
+indice=0
+
+for letra in palabra:
+    print(letra,indice)
+    indice=indice + 1 
+
+marcas= ["bmw","honda","nissan"]
+
+for marca in marcas:
+  print(marca)
+
+for indice,letra in enumerate(palabra):
+    print(indice,letra)
---
+++ pokemon.py
@@ -0,0 +1,17 @@
+import requests,json
+
+url = "https://pokeapi.co/api/v2/pokemon"
+
+response=requests.request ("get", url)
+
+# print(response.text)
+
+mi_diccionario= json.loads(response.text)
+
+print(mi_diccionario["results"][0])
+
+resultados = mi_diccionario["results"]
+
+
+for resultado in resultados:
+    print (resultado)
